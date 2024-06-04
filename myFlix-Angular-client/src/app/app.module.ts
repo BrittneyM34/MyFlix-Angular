cd5d97c3b3416423c3a1f13085e3788d5a11c99d
BrittneyM34 <brittneym1018@yahoo.com>
2024-06-04 09:43:45 -0400

    add mat icon

--- myFlix-Angular-client/src/app/app.module.ts
+++ myFlix-Angular-client/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -45,6 +46,7 @@ const appRoutes: Routes = [
     MatFormFieldModule,
     MatDialogModule,
     MatSnackBarModule,
+    MatIconModule,
     FormsModule
   ],
   providers: [],
